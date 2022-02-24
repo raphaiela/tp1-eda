9ee1663de063d9ceef4de88b3c3d19994f73867f
Raphaela <rapha.ela12@hotmail.com>
2022-02-24 08:28:00 -0300

    .

--- manipula_corpus.h
+++ manipula_corpus.h
@@ -1,5 +1,6 @@
 //BIBLIOTECA DE MANIPULAÇÃO DOS ARQUIVOS TXT CRIADOS
 #include <stdio.h>
+#include <string.h>
 
 void cria_arquivos(){
     FILE *fp = NULL;
@@ -9,6 +10,39 @@ void cria_arquivos(){
     fp = fopen("Nota4.txt" ,"w");
     fp = fopen("Nota5.txt" ,"w");
 }
+
 void aglutina(FILE *f, char *nome_do_arquivo){
-    FILE *f = fopen(nome_do_arquivo, "r");
+    char buffer[100000];
+        int row = 0;
+        int column = 0;
+    while (fgets(buffer, 100000, f)) {
+    
+            column = 0;
+            row++;
+            if (row == 1)
+                continue;
+    
+            char* value = strtok(buffer, "\n");
+  
+            while (value) {
+        
+                // Column 1
+                if (column == 0) {
+                    printf("review :");
+                }
+  
+                // Column 2
+                if (column == 1) {
+                    printf("\nnota :");
+                }
+  
+                printf("%s", value);
+                printf("nota=");
+                printf(value[strlen(value)-1]);
+                value = strtok(NULL, "\n");
+                column++;
+            }
+  
+            printf("\n");
+        }
 }
--- teste.c
+++ teste.c
@@ -1,45 +1,11 @@
-#include <conio.h>
-#include <stdio.h>
-#include <string.h>
-  
-// Driver Code
-int main()
+//arquivo temporário para testar funções
+#include "manipula_corpus.h"
+
+int main(void)
 {
-    // Substitute the full file path
-    // for the string file_path
-    FILE* fp = fopen(nome_do_arquivo, "r"); //Open CSV File using File Pointer.
-  
-    if (!fp)
-        printf("Can't open file\n");
-  
-    else {
-        // Here we have taken size of
-        // array 1024 you can modify it
-        char buffer[1000000000]; //Extract the whole file data into a char buffer array.
-        //Now initialize row and column variables with value 0.
-        int row = 0;
-        int column = 0;
-  
-        while (fgets(buffer, 1000000000 fp)) { 
-            row++; 
-  
-            // To avoid printing of column
-            // names in file can be changed
-            // according to need
-            if (row == 1)
-                continue;
-  
-            // Splitting the data
-            char* value = strtok(buffer, "; ");
-  
-            while (value) {
-                printf("%s", value);
-                value = strtok(NULL, "; "); //Print data separated by a comma and increase the column variable.
-            }
-        }
-  
-        // Close the file
-        fclose(fp);
-    }
-    return 0;
+    char *small = "tripadvisor_hotel_reviews.csv";
+    printf("linha 6");
+    FILE* fp = fopen(small, "r");
+    printf("linha 8");
+    aglutina(fp, small);
 }
